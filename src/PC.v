da95a07205ae842024018724795ddee6601a156a
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2019-12-10 07:35:46 +0800

    fix "inferring latch" in PC

--- src/PC.v
+++ src/PC.v
@@ -12,42 +12,19 @@ module PC (
     input wire                  pcJump_in,
     input wire[`addrRange]      pcTarget_in,
 
-    // output to IF_ID
+    // output to IF
     output reg[`addrRange]      pc_out
 );
-
-    reg pcJump;
-    reg Jump;
-    reg[`addrRange] pcTarget;
-
-    always @ (*) begin
-        if (rst_in == `rstEnable) begin
-            pcJump      <= `NoJump;
-            pcTarget    <= `ZERO32;
-        end else if (pcJump_in == `Jump) begin
-            pcJump      <= `Jump;
-            pcTarget    <= pcTarget_in;
-        end else if (Jump == 1) begin
-            pcJump      <= `NoJump;
-            pcTarget    <= `ZERO32;
-        end else begin
-            // do nothing
-        end
-    end
 
     always @ (posedge clk_in) begin
         if (rst_in == `rstEnable) begin
             pc_out <= `ZERO32;
-            Jump <= 0;
         end else if (stall_in[0] == `NoStall) begin
-            if (pcJump == `Jump) begin
-                pc_out <= pcTarget;
+            if (pcJump_in == `Jump) begin
+                pc_out <= pcTarget_in;
             end else begin
                 pc_out <= pc_out + `PCSTEP;
             end
-            Jump <= 1;
-        end else begin
-            Jump <= 0;
         end
     end
 
